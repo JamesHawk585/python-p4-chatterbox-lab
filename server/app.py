9ea94b708d027fcf7eef4c5cf67d46a0a2ad6e2c
James Hawk <jph94880@gmail.com>
2023-09-15 14:35:10 -0500

    Adds message variable declaration to the DELETE method in messages_by_id().

--- server/app.py
+++ server/app.py
@@ -15,28 +15,34 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
-
+@app.route('/')
+def index():
+     return '<h1>Hello!</h1>'
+      
 # [x] GET /messages: returns an array of all messages as JSON, ordered by created_at in ascending order.
 # [] POST /messages: creates a new message with a body and username from params, and returns the newly created post as JSON.
 
 @app.route('/messages', methods=['GET', 'POST'])
 def messages():
+
     if request.method == 'GET':
-            messages = Message.query.all()
-            messages_dict = [message.to_dict() for message in messages]
+        messages = []
+        for message in Message.query.all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
 
-            response = make_response(
-                messages_dict, 
-                200
-            )
+        response = make_response(
+            messages,
+            200
+        )
 
-            return response
+        return response 
     
     elif request.method == 'POST':
         new_message = Message(
-            id = request.form.get("id"), 
+            id = request.form.get("id"),
             body = request.form.get("body"),
-            username = request.form.get("username"),
+            username = request.form.get("username")
         )
 
         db.session.add(new_message)
@@ -49,14 +55,46 @@ def messages():
             201
         )
 
-        return response 
+        return response
 
 # PATCH /messages/<int:id>: updates the body of the message using params, and returns the updated message as JSON.
 # DELETE /messages/<int:id>: deletes the message from the database.
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    
+    if request.method == 'PATCH':
+        message = Message.query.filter(Message.id == id).first()
+        for attr in request.form:
+            setattr(message, attr, request.form.get(attr))
+            db.session.add(message)
+            db.session.commit()
+            message_serialized = message.to_dict()
+
+            response = make_response(
+                message_serialized,
+                200
+            )
+
+            return response
+        
+    elif request.method == 'DELETE':
+        message = Message.query.filter(Message.id == id).first()
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Bakery Deleted"
+        }
+
+        response_body = make_response(
+              response_body,
+              200
+         )
+        
+        return response
+         
 
 if __name__ == '__main__':
     app.run(port=5555)
